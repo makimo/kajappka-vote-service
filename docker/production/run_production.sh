26925d8df161574585fbfbbe189b2e393d8ad16a
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-09-17 08:19:05 +0200

    Fix production script
    Add missing flask env variables

--- docker/production/run_production.sh
+++ docker/production/run_production.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+PYTHONDIR="/app"
 NAME='votes-service'
 APP='votes:create_app()'
 NUM_WORKERS=5
 PORT=":8080"
 
-export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+export PYTHONPATH=$PYTHONDIR
+export FLASK_APP=votes
+export FLASK_ENV=production
 
 exec gunicorn $APP \
     --name $NAME \
